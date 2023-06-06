00c2cb98cad4c4b412f93d651639ab5588398735
Ante Kraljević <86553387+Akraljevic@users.noreply.github.com>
2023-06-06 16:15:50 +0200

    Add files via upload

---
+++ src/pages/HelpItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="q-pa-lg">
+  <h3 class="q-mt-none">Help</h3>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At commodi quas, possimus deleniti, eveniet necessitatibus amet totam veniam ad culpa deserunt, praesentium maxime nisi sapiente aspernatur. Perferendis temporibus quo exercitationem.</p>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At commodi quas, possimus deleniti, eveniet necessitatibus amet totam veniam ad culpa deserunt, praesentium maxime nisi sapiente aspernatur. Perferendis temporibus quo exercitationem.</p>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At commodi quas, possimus deleniti, eveniet necessitatibus amet totam veniam ad culpa deserunt, praesentium maxime nisi sapiente aspernatur. Perferendis temporibus quo exercitationem.</p>
+
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  
+})
+</script>
